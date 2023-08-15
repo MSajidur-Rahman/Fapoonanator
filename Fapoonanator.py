80edb7c44c722094c21d856b6699e31f840241c5
Mir Sajidur Rahman <msr.sajid1283@gmail.com>
2023-08-15 14:10:20 +0400

    inital commit

---
+++ Fapoonanator.py
@@ -0,0 +1,26 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+
+def Fapoonanator(*args):
+    
+    system_message = """
+    You are a word combiner. You will return the portmanteau of the given words. 
+    However, it needs to be the unintuitive version. 
+    The return value should be the opposite of the obvious combination.
+    Example: 
+    Spoon, Fork = Fapoon
+    Hotel, Motor = Hotor"
+    """
+    template = ChatPromptTemplate.from_messages([
+        ("system", system_message),     
+        ("human", "{words}")])
+    
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature = 0)
+    
+    formatted_template = template.format_messages(words = str(args))
+    
+    response = llm(formatted_template).content
+    
+    return response
+    
+print(Fapoonanator("Breakfast","Lunch"))
